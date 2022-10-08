4183b50b90be8d0c5948e8b192b3dd81c51dd536
Terri Read <trrwebdev@gmail.com>
2022-10-08 12:12:23 -0500

    second commit

--- src/features/comments/commentsSlice.js
+++ src/features/comments/commentsSlice.js
@@ -3,5 +3,5 @@ import { COMMENTS } from '../../app/shared/COMMENTS';
 export const selectCommentsByCampsiteId = (campsiteId) => {
     return COMMENTS.filter(
         (comment) => comment.campsiteId === parseInt(campsiteId)
-    );
+    )
 };
