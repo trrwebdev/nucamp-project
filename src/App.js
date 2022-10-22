cbdbccffc50d0ae3a9a57278da05e9b6cfb47a40
Terri Read <trrwebdev@gmail.com>
2022-10-22 12:04:21 -0500

    Async Partners

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import './App.css';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { fetchCampsites } from './features/campsites/campsitesSlice';
+import { fetchPartners } from './features/partners/partnersSlice';
 
 
 
@@ -18,6 +19,7 @@ function App() {
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(fetchCampsites());
+        dispatch(fetchPartners());
     }, [dispatch]);
     return (
         <div className='App'>
